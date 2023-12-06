1bb86b7462bd2729c0aa71bc2d382399d2beb0c2
Gerrard Sebastian <114888829+gerrardgs@users.noreply.github.com>
2023-12-06 17:36:19 +0700

    Create sourceCode24.java

---
+++ javaCollege/sourceCode24.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int x = 10;
+        System.out.println((x));
+        String str = "Hello";
+        System.out.println(str.toUpperCase());
+
+    }
+}
